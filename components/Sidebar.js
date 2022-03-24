380f92d32017d1d2ed3d2e2b4caff3bb7f119d74
Hernán D. Mercado <hernandmf@gmail.com>
2022-03-24 15:58:28 -0500

    getting information from the API

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import useQuiosco from "../hooks/useQuiosco";
+import Categoria from "./Categoria";
 
 const Sidebar = () => {
+  const { categorias } = useQuiosco();
   return (
     <>
       <Image
@@ -9,6 +12,11 @@ const Sidebar = () => {
         src={"/assets/img/logo.svg"}
         alt="logotipo"
       />
+      <nav className="mt-10">
+        {categorias.map((categoria) => (
+          <Categoria key={categoria.id} categoria={categoria} />
+        ))}
+      </nav>
     </>
   );
 };
--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,10 +1,27 @@
 import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 const QuioscoContext = createContext();
 
 const QuioscoProvider = ({ children }) => {
+  const [categorias, setCategorias] = useState([]);
+
+  const obtenerCategorias = async () => {
+    try {
+      const url = "/api/categorias";
+      const { data } = await axios(url);
+      setCategorias(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    obtenerCategorias();
+  }, []);
   return (
-    <QuioscoContext.Provider value={{}}>{children}</QuioscoContext.Provider>
+    <QuioscoContext.Provider value={{ categorias }}>
+      {children}
+    </QuioscoContext.Provider>
   );
 };
 
