d4936b2e6c926d3bfaddb69d10a8067cdffc8a9c
Hernán D. Mercado <hernandmf@gmail.com>
2022-03-24 11:55:40 -0500

    querying the DB with nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
-export default function Home() {
+import { PrismaClient } from "@prisma/client";
+
+export default function Home({ categorias }) {
+  console.log(categorias);
   return <h1>Next JS</h1>;
 }
+
+// Getting the DB data
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+
+  const categorias = await prisma.categoria.findMany();
+
+  return {
+    props: {
+      categorias,
+    },
+  };
+};
