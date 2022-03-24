18677986c435004d746990f357dbd245e99f3ce2
Hernán D. Mercado <hernandmf@gmail.com>
2022-03-24 17:36:48 -0500

    eager loading in prisma

--- pages/api/productos.js
+++ pages/api/productos.js
@@ -3,11 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  const categorias = await prisma.categoria.findMany({
-    include: {
-      productos: true,
+  const productos = await prisma.producto.findMany({
+    where: {
+      categoriaId: 1,
     },
   });
 
-  res.status(200).json(categorias);
+  res.status(200).json(productos);
 }
